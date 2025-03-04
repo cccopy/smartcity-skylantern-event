2a6843c0c94eb3d06cf253c6ec8c13d848d64414
KinoLien <miecowbai@gmail.com>
2025-03-04 14:45:46 +0800

    Make the page to index and use html2canvas and filesaver

--- js/float_away.js
+++ js/float_away.js
@@ -1,6 +1,8 @@
 
 $(document).ready(() => {
-    var infoWrap = $(".lantern-info");
+    var mainContent = $("main.bg-skycity");
+    var loadingContent = $("main.bg-city");
+    var infoWrap = $("main.bg-skycity .lantern-info");
     
     // assign name
     infoWrap.find("h3.name").text(localStorage.getItem('name'));
@@ -12,4 +14,18 @@ $(document).ready(() => {
     var strings = JSON.parse( localStorage.getItem('lanternText') );
     infoWrap.find(".lantern-text h4").text(strings[0]);
     infoWrap.find(".lantern-text p").text(strings[1]);
+
+    setTimeout(() => {
+        loadingContent.hide();
+        mainContent.show();
+    }, 3500);
 });
+
+function onDownloadClick() {
+    var infoWrap = $("main.bg-skycity .lantern-info");
+    infoWrap.find("h3.name").css('box-shadow', 'none');
+    html2canvas(infoWrap.get(0)).then(canvas => {
+        document.body.appendChild(canvas);
+        saveAs(canvas.toDataURL('image/png'), "image.png");
+    });
+}
